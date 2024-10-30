620dfa605c46bfed8a2e2ebb59f5198c496fa2ee
lixin09 <lixin09@zhihu.com>
2024-10-30 10:59:56 +0800

    new commit in 2024/10/30

--- day_14/14_1_DigitalRecongnition.py
+++ day_14/14_1_DigitalRecongnition.py
@@ -26,6 +26,11 @@ model.compile(
     metrics=['accuracy'],
 )
 
+print(f'x_train shape: {x_train.shape}')
+print(f'x_test shape: {x_test.shape}')
+print(f'y_train shape: {y_train.shape}')
+print(f'y_test shape: {y_test.shape}')
+
 history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
 
 test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
