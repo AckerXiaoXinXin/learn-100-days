6e3bc6432c04399fd408960b29dcce020d575ace
lixin09 <lixin09@zhihu.com>
2024-10-30 10:52:43 +0800

    new commit in 2024/10/30

--- day_14/14_1_DigitalRecongnition.py
+++ day_14/14_1_DigitalRecongnition.py
@@ -2,17 +2,16 @@ import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.keras.metrics import SparseTopKCategoricalAccuracy
+from tensorflow.keras.utils import to_categorical
 
 
 (x_train, x_test), (y_train, y_test) = mnist.load_data()
-x_train = x_train/255
-x_test = x_test/255
+x_train, x_test = x_train/255, x_test/255
 
 x_train.astype('float32')
 x_test.astype('float32')
+
+y_train, y_test = to_categorical(y_train), to_categorical(y_test)
 
 model = Sequential([
     Flatten(input_shape=(28, 28)),
@@ -22,12 +21,12 @@ model = Sequential([
 ])
 
 model.compile(
-    optimizer=Adam(),
-    loss=SparseCategoricalCrossentropy(),
-    metrics=[SparseTopKCategoricalAccuracy],
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy'],
 )
 
-history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.1)
+history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
 
 test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
 print(f'test loss: {test_loss:}, test accuracy: {test_acc:.2f}')
