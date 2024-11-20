aea6ba063bb800a1dfba740cbe953beb82c799ea
lixin09 <lixin09@zhihu.com>
2024-11-20 18:26:57 +0800

    new commit in 2024/11/20-1

--- 4_DL/day_40/40_1_StartTest.py
+++ 4_DL/day_40/40_1_StartTest.py
@@ -2,9 +2,12 @@ import torch
 
 
 if __name__ == '__main__':
-    # x = torch.arange(12, dtype=torch.float32)
-    # print(x)
-    # print(x.numel())
+    x = torch.arange(12, dtype=torch.float32)
+    print(x)
+    print(x.numel())
+    print(x.shape(12))
+
     y = torch.arange(24, dtype=torch.float)
     print(y)
     print(y.numel())
+    print(x.shape(12))
